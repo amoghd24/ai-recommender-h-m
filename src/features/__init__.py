16f7901842cc1cd37a1e632984ccd2446bf86d27
Amogh Dikshit <amoghdikshit24@gmail.com>
2025-07-02 17:17:51 -0700

    interaction features

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,3 +1,10 @@
 """
 Feature engineering modules for the H&M recommender system.
-""" 
+"""
+
+from .customer_features import CustomerFeatureEngineer
+from .article_features import ArticleFeatureEngineer
+from .temporal_features import TemporalFeatureEngineer
+from .interaction_features import InteractionFeatureEngineer
+
+__all__ = ['CustomerFeatureEngineer', 'ArticleFeatureEngineer', 'TemporalFeatureEngineer', 'InteractionFeatureEngineer'] 
